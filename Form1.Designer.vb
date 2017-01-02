385b3fbec4501fd30cbf8d17cb89812542b7bcd9
selmyfulgan <selmyfulgan@gmail.com>
2017-01-03 00:16:49 +0100

    matriz 3 x 3 nivel basico

---
+++ Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.b1 = New System.Windows.Forms.Button()
+        Me.b2 = New System.Windows.Forms.Button()
+        Me.b3 = New System.Windows.Forms.Button()
+        Me.b6 = New System.Windows.Forms.Button()
+        Me.b5 = New System.Windows.Forms.Button()
+        Me.b4 = New System.Windows.Forms.Button()
+        Me.b9 = New System.Windows.Forms.Button()
+        Me.b8 = New System.Windows.Forms.Button()
+        Me.b7 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'b1
+        '
+        Me.b1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b1.Location = New System.Drawing.Point(99, 76)
+        Me.b1.Name = "b1"
+        Me.b1.Size = New System.Drawing.Size(90, 74)
+        Me.b1.TabIndex = 0
+        Me.b1.UseVisualStyleBackColor = True
+        '
+        'b2
+        '
+        Me.b2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b2.Location = New System.Drawing.Point(234, 76)
+        Me.b2.Name = "b2"
+        Me.b2.Size = New System.Drawing.Size(90, 74)
+        Me.b2.TabIndex = 1
+        Me.b2.UseVisualStyleBackColor = True
+        '
+        'b3
+        '
+        Me.b3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b3.Location = New System.Drawing.Point(367, 76)
+        Me.b3.Name = "b3"
+        Me.b3.Size = New System.Drawing.Size(90, 74)
+        Me.b3.TabIndex = 2
+        Me.b3.UseVisualStyleBackColor = True
+        '
+        'b6
+        '
+        Me.b6.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b6.Location = New System.Drawing.Point(367, 176)
+        Me.b6.Name = "b6"
+        Me.b6.Size = New System.Drawing.Size(90, 74)
+        Me.b6.TabIndex = 5
+        Me.b6.UseVisualStyleBackColor = True
+        '
+        'b5
+        '
+        Me.b5.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b5.Location = New System.Drawing.Point(234, 176)
+        Me.b5.Name = "b5"
+        Me.b5.Size = New System.Drawing.Size(90, 74)
+        Me.b5.TabIndex = 4
+        Me.b5.UseVisualStyleBackColor = True
+        '
+        'b4
+        '
+        Me.b4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b4.Location = New System.Drawing.Point(99, 176)
+        Me.b4.Name = "b4"
+        Me.b4.Size = New System.Drawing.Size(90, 74)
+        Me.b4.TabIndex = 3
+        Me.b4.UseVisualStyleBackColor = True
+        '
+        'b9
+        '
+        Me.b9.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b9.Location = New System.Drawing.Point(367, 274)
+        Me.b9.Name = "b9"
+        Me.b9.Size = New System.Drawing.Size(90, 74)
+        Me.b9.TabIndex = 8
+        Me.b9.UseVisualStyleBackColor = True
+        '
+        'b8
+        '
+        Me.b8.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b8.Location = New System.Drawing.Point(234, 274)
+        Me.b8.Name = "b8"
+        Me.b8.Size = New System.Drawing.Size(90, 74)
+        Me.b8.TabIndex = 7
+        Me.b8.UseVisualStyleBackColor = True
+        '
+        'b7
+        '
+        Me.b7.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b7.Location = New System.Drawing.Point(99, 274)
+        Me.b7.Name = "b7"
+        Me.b7.Size = New System.Drawing.Size(90, 74)
+        Me.b7.TabIndex = 6
+        Me.b7.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Font = New System.Drawing.Font("Showcard Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button10.Location = New System.Drawing.Point(199, 472)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(141, 38)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "Reiniciar"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.Font = New System.Drawing.Font("Showcard Gothic", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Yellow
+        Me.Label1.Location = New System.Drawing.Point(50, 390)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 37)
+        Me.Label1.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImage = Global.totito.My.Resources.Resources.Video_juego_Gold_Minecraft_480826
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(543, 522)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.b9)
+        Me.Controls.Add(Me.b8)
+        Me.Controls.Add(Me.b7)
+        Me.Controls.Add(Me.b6)
+        Me.Controls.Add(Me.b5)
+        Me.Controls.Add(Me.b4)
+        Me.Controls.Add(Me.b3)
+        Me.Controls.Add(Me.b2)
+        Me.Controls.Add(Me.b1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TOTITO"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents b1 As Button
+    Friend WithEvents b2 As Button
+    Friend WithEvents b3 As Button
+    Friend WithEvents b6 As Button
+    Friend WithEvents b5 As Button
+    Friend WithEvents b4 As Button
+    Friend WithEvents b9 As Button
+    Friend WithEvents b8 As Button
+    Friend WithEvents b7 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,167 @@
+﻿Public Class Form1
+    Dim c As New codigo
+    Dim nGanador As Integer = -1
+    Dim nTablero(2, 2) As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        c.empezarPartida()
+        nTablero = c.getTablero
+    End Sub
+    Private Sub checkGanador()
+        Dim ub(2) As Integer
+        ub = c.getUltimoBoton
+        If ub(0) = 0 And ub(1) = 0 Then
+            b1.Text = "O"
+        End If
+        If ub(0) = 0 And ub(1) = 1 Then
+            b2.Text = "O"
+        End If
+        If ub(0) = 0 And ub(1) = 2 Then
+            b3.Text = "O"
+        End If
+        If ub(0) = 1 And ub(1) = 0 Then
+            b4.Text = "O"
+        End If
+        If ub(0) = 1 And ub(1) = 1 Then
+            b5.Text = "O"
+        End If
+        If ub(0) = 1 And ub(1) = 2 Then
+            b6.Text = "O"
+        End If
+        If ub(0) = 2 And ub(1) = 0 Then
+            b7.Text = "O"
+        End If
+        If ub(0) = 2 And ub(1) = 1 Then
+            b8.Text = "O"
+        End If
+        If ub(0) = 2 And ub(1) = 2 Then
+            b9.Text = "O"
+        End If
+        If nGanador = 0 Then
+            Label1.Text = "Felicidades has ganado!!!"
+        End If
+        If nGanador = 1 Then
+            Label1.Text = "Que lo siento has perdido :("
+        End If
+        If nGanador = -1 Then
+            If c.tableroCompleto() = True Then
+                Label1.Text = "Empate"
+            End If
+        End If
+    End Sub
+
+    Private Sub b1_Click(sender As Object, e As EventArgs) Handles b1.Click
+        If nTablero(0, 0) = -1 Then
+            b1.Text = "X"
+            c.pulsaBoton(0, 0)
+            nGanador = c.ganarPartida()
+            Call checkGanador()
+            b1.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub b2_Click(sender As Object, e As EventArgs) Handles b2.Click
+        If nTablero(0, 1) = -1 Then
+            b2.Text = "X"
+            c.pulsaBoton(0, 1)
+            nGanador = c.ganarPartida()
+            Call checkGanador()
+            b1.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub b3_Click(sender As Object, e As EventArgs) Handles b3.Click
+        If nTablero(0, 2) = -1 Then
+            b3.Text = "X"
+            c.pulsaBoton(0, 2)
+            nGanador = c.ganarPartida()
+            Call checkGanador()
+            b3.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub b4_Click(sender As Object, e As EventArgs) Handles b4.Click
+        If nTablero(1, 0) = -1 Then
+            b4.Text = "X"
+            c.pulsaBoton(1, 0)
+            nGanador = c.ganarPartida()
+            Call checkGanador()
+            b4.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub b5_Click(sender As Object, e As EventArgs) Handles b5.Click
+        If nTablero(1, 1) = -1 Then
+            b5.Text = "X"
+            c.pulsaBoton(1, 1)
+            nGanador = c.ganarPartida()
+            Call checkGanador()
+            b5.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub b6_Click(sender As Object, e As EventArgs) Handles b6.Click
+        If nTablero(1, 2) = -1 Then
+            b6.Text = "X"
+            c.pulsaBoton(1, 2)
+            nGanador = c.ganarPartida()
+            Call checkGanador()
+            b6.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub b7_Click(sender As Object, e As EventArgs) Handles b7.Click
+        If nTablero(2, 0) = -1 Then
+            b7.Text = "X"
+            c.pulsaBoton(2, 0)
+            nGanador = c.ganarPartida()
+            Call checkGanador()
+            b7.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub b8_Click(sender As Object, e As EventArgs) Handles b8.Click
+        If nTablero(2, 1) = -1 Then
+            b8.Text = "X"
+            c.pulsaBoton(2, 1)
+            nGanador = c.ganarPartida()
+            Call checkGanador()
+            b8.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub b9_Click(sender As Object, e As EventArgs) Handles b9.Click
+        If nTablero(2, 2) = -1 Then
+            b9.Text = "X"
+            c.pulsaBoton(2, 2)
+            nGanador = c.ganarPartida()
+            Call checkGanador()
+            b9.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        c = New codigo
+        c.empezarPartida()
+        nTablero = c.getTablero
+        nGanador = -1
+        b1.Text = ""
+        b2.Text = ""
+        b3.Text = ""
+        b4.Text = ""
+        b5.Text = ""
+        b6.Text = ""
+        b7.Text = ""
+        b8.Text = ""
+        b9.Text = ""
+        Label1.Text = ""
+        b1.ForeColor = Color.Black
+        b2.ForeColor = Color.Black
+        b3.ForeColor = Color.Black
+        b4.ForeColor = Color.Black
+        b5.ForeColor = Color.Black
+        b6.ForeColor = Color.Black
+        b7.ForeColor = Color.Black
+        b8.ForeColor = Color.Black
+        b9.ForeColor = Color.Black
+    End Sub
+End Class
